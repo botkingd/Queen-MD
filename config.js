4b5b8ed1a86d4f947446c64a2500c920ea9c3933
botkingd <119658836+botkingd@users.noreply.github.com>
2023-03-06 10:30:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,15 +21,15 @@ global.APIKeys = {
 }
 ////////////////////////////////////////////////////|:|             OWNER DETAILS             |:|//////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-global.session = process.env.SESSION_ID || 'PUT HERE',
-global.ownername = process.env.OWNER_NAME === undefined ? "DarkWinzo" : process.env.OWNER_NAME
+global.session = process.env.SESSION_ID || 'ASTRO=Q6ZGHSJa#wi2IXlYRHEu9tRTBLjoiuV6Ud1Mbm_V2D89J7V_zids',
+global.ownername = process.env.OWNER_NAME === undefined ? "KINGDOM ZEUS" : process.env.OWNER_NAME
 let fake = process.env.OWNER_NUMBER === undefined ? '94775200935' : process.env.OWNER_NUMBER
 global.owner = [`${fake}`]
 global.ownernumber = [`${fake}`]
 global.premium = [`${fake}`]
 global.anticall = process.env.ANTICALL === undefined ? 'false' : process.env.ANTICALL
-global.packname = process.env.PACK_NAME === undefined ? 'Queen-MD' : process.env.PACK_NAME
-global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'DarkWinzo' : process.env.STICKER_AUTHOR_NAME
+global.packname = process.env.PACK_NAME === undefined ? 'KINGDDOM ZEUS ' : process.env.PACK_NAME
+global.author = process.env.STICKER_AUTHOR_NAME === undefined ? 'PIUSHA' : process.env.STICKER_AUTHOR_NAME
 global.pmblock = process.env.PM_BLOCK === undefined ? 'false' : process.env.PM_BLOCK
 global.chatbot_pm = process.env.PM_CHATBOT === undefined ? 'false' : process.env.PM_CHATBOT
 global.chatbot_group = process.env.GROUP_CHATBOT === undefined ? 'false' : process.env.GROUP_CHATBOT
